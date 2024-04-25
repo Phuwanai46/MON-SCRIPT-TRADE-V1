cec3bb3cdc91cec11db8340c90cff6877b78b4b3
Phuwanai46 <107763756+Phuwanai46@users.noreply.github.com>
2024-04-25 08:41:58 +0700

    Update MON.lua

--- MON.lua
+++ MON.lua
@@ -1,10 +1,5 @@
--- Create BY MON
--- MON SCRIPT
-
--- R and S
-
 instrument {
-    name = 'Support and resistance barry',
+    name = 'MON SCRIPT V1',
     icon = 'indicators:RSI',
     overlay = True
 }
@@ -32,7 +27,6 @@ plot (RSTB, "support", iff(RSTB ~= RSTB[1], na ,bottom_color), 4, 0, style.level
     overlay = true
 }
 
--- SMA3
 
 MaFast_period = input(3,"Ma Fast period",input.integer,1,1000,1)
 MaFast_average = input(1,"Ma Fast average", input.string_selection,averages.titles)
@@ -45,8 +39,6 @@ MaSlow_title = input(1,"Ma Slow title", input.string_selection,inputs.titles)
 MaTrend_period = input(100,"Ma Trend period",input.integer,1,1000,5)
 MaTrend_average = input(2,"Ma Trend average", input.string_selection,averages.titles)
 MaTrend_title = input(1,"Ma Trend title", input.string_selection,inputs.titles)
-
--- Create BY MON
 
 input_group {
     "Ma Fast Line",
@@ -99,8 +91,6 @@ filter_pa_index = input(8,"Candle check resolution",input.string_selection,filte
 
 filter_pa = security (current_ticker_id, filter_source[filter_pa_index])
 
--- SMA5
-
 instrument {
     name = 'SMA5',
     short_name = 'SMA5',
@@ -127,7 +117,6 @@ input_group {
 
 local candleValue = inputs[CANDLE_VALUE]
 
--- Moving Average EMA
 emaValue = ema(candleValue, EMA_PERIOD)
 input_group {
     "front.middle line",
@@ -136,7 +125,6 @@ input_group {
     middle_line_width   = input { default = 1, type = input.line_width }
 }
 
--- Moving Average SMA aux
 smaValueAux = sma(candleValue, SMA_AUX_PERIOD)
 
 plot (emaValue, "Middle", middle_line_color, middle_line_width)
@@ -153,8 +141,6 @@ SELL_CONDITION = conditional((emaValue < smaValueAux and emaValue < smaValueAux[
     overlay = true
 }
 
--- SMA5
--- Create BY MON
 
 MaFast_period = input(5,"Ma Fast period",input.integer,1,1000,1)
 MaFast_average = input(1,"Ma Fast average", input.string_selection,averages.titles)
@@ -167,8 +153,6 @@ MaSlow_title = input(1,"Ma Slow title", input.string_selection,inputs.titles)
 MaTrend_period = input(100,"Ma Trend period",input.integer,1,1000,5)
 MaTrend_average = input(2,"Ma Trend average", input.string_selection,averages.titles)
 MaTrend_title = input(1,"Ma Trend title", input.string_selection,inputs.titles)
-
--- Create BY MON
 
 input_group {
     "Ma Fast Line",
@@ -264,9 +248,6 @@ instrument {
     overlay =true
 }
 
--- Create BY MON
--- Name "MON Quiet"
-
 local N = modf(5/2)
 
 local smaLine = sma(close, 5)
@@ -280,14 +261,10 @@ BearishFractal = high[N]  > high[N-2] and
 BullishFractal = low[N] < low[N-2] and 
                  low[N] < low[N-1] and
                  low[N] < low[N+1] and
-                 low[N] < low[N+2] 
- --Plot Fractal   
-plot_shape(BearishFractal, "BearishFractal", shape_style.triangledown, shape_size.large, "white", shape_location.abovebar, -N, "mon sell", "white") 
-plot_shape(BullishFractal, "BullishFractal", shape_style.triangleup, shape_size.large, "white", shape_location.belowbar, -N, "mon buy", "yellow")
+                 low[N] < low[N+2]  
+plot_shape(BearishFractal, "BearishFractal", shape_style.triangledown, shape_size.large, "white", shape_location.abovebar, -N, "mon sell", "red") 
+plot_shape(BullishFractal, "BullishFractal", shape_style.triangleup, shape_size.large, "white", shape_location.belowbar, -N, "mon buy", "green")
 
 
 buy  = BullishFractal and close > smaLine 
 sell = BearishFractal and close < smaLine
-  
--- Create BY MON
--- Thank you
